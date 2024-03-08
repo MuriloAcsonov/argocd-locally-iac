a66664d78ef59220c6223ce584f23ba539ab6184
Murilo Acsonov <muriloacsonov@eth2-2-lb-ha.markiza.sk>
2024-03-08 17:18:13 -0300

    create stuff

--- with_bash/configure_argo.sh
+++ with_bash/configure_argo.sh
@@ -6,5 +6,6 @@ ARGOCD_PASS=$(kubectl get secret -n argocd argocd-initial-admin-secret -o jsonpa
 argocd login localhost:30000 --insecure --username admin --password $ARGOCD_PASS
 
 echo -e "Exporting config to new cluster on argo\n"
-argocd app create nginx --auto-prune --repo https://github.com/MuriloAcsonov/argocd-locally-iac --path ./manifests --dest-server https://kubernetes.default.svc --dest-namespace default --revision main --sync-policy auto 
-argocd app create nginx --auto-prune --repo https://github.com/MuriloAcsonov/argocd-locally-iac --path ./manifests --dest-server https://k3d-dev-server-0:6443 --dest-namespace default --revision main --sync-policy auto
+kubectl apply -f ../applications/apps.yaml
+#argocd app create nginx --auto-prune --repo https://github.com/MuriloAcsonov/argocd-locally-iac --path ./manifests --dest-server https://kubernetes.default.svc --dest-namespace default --revision main --sync-policy auto 
+#argocd app create nginx --auto-prune --repo https://github.com/MuriloAcsonov/argocd-locally-iac --path ./manifests --dest-server https://k3d-dev-server-0:6443 --dest-namespace default --revision main --sync-policy auto
